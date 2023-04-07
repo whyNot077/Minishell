9b9a72b6f549fb94b22eb60c1b3768fb9d59a441
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-07 13:00:05 +0900

    readline

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,13 +6,33 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 14:37:30 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/06 19:41:52 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/07 12:59:17 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int main()
+char *read_input(void)
 {
-	ft_printf("Hello World\n");
+    char	*input;
+
+	input = readline(" ");
+    if (input && *input)
+        add_history(input);
+    return (input);
+}
+
+int	main()
+{
+    char	*input;
+
+    while (1)
+	{
+		printf("%sminishell >%s", BLUE, DEFAULT);
+        input = read_input();
+		if (input == NULL)
+			break ;
+    }
+
+    return 0;
 }
