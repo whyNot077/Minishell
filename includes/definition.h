50fbd813b639821d18a2d1edcdb3af3cc63dd61e
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-07 15:49:30 +0900

    add header.h and definition.h

--- includes/definition.h
+++ includes/definition.h
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   definition.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/06 19:26:53 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/07 15:48:44 by minkim3          ###   ########.fr       */
+/*   Created: 2023/04/07 15:47:16 by minkim3           #+#    #+#             */
+/*   Updated: 2023/04/07 15:48:20 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef DEFINITION_H
+# define DEFINITION_H
 
-# include "header.h"
-# include "definition.h"
+# define RED "\033[1;31m"
+# define PINK "\033[1;35m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define CYAN "\033[1;36m"
+# define DEFAULT "\033[0m"
+
+enum e_definition
+{
+	ERROR = -1
+};
 
 #endif
---
+++ includes/header.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/07 15:47:19 by minkim3           #+#    #+#             */
+/*   Updated: 2023/04/07 15:47:34 by minkim3          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <signal.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <term.h>
+# include <termios.h>
+# include <sys/wait.h>
+# include <sys/resource.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "parsing.h"
+# include "execute.h"
+# include "built_in.h"
+# include "../libft/libft.h"
+# include "binary_tree.h"
+# include "main.h"
+
+#endif
