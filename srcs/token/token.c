872a6378ce86e65264d7fca894ea81964ed71f16
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-10 14:24:43 +0900

    fix is_space functioc

--- srcs/token/token.c
+++ srcs/token/token.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 17:09:12 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/10 14:07:46 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/10 14:24:07 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,7 @@ int	is_operator(char c)
 
 int	is_space(char c)
 {
-	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f'
-		|| c == '\r');
+	return (c == ' ');
 }
 
 void	type_of_token(t_token *tokens, int token_index)
