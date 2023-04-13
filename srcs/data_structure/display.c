36927a1eb70de5b2518ba2a16d5c28befe60b5ad
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-13 21:24:23 +0900

    display

--- srcs/data_structure/display.c
+++ srcs/data_structure/display.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 14:16:53 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/13 19:10:57 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/13 21:23:05 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ void	display_tree(tree_node *node)
 	{
 		printf("Executing: %s\n", node->data);
 	}
+	else if (node->type == PIPE)
+		printf("PIPE = %s\n", node->data);
+	else if (node->type == REDIRECT_OUT)
+		printf("REDIRECT_OUT = %s\n", node->data);
+	else if (node->type == REDIRECT_IN)
+		printf("REDIRECT_IN = %s\n", node->data);
 	display_tree(node->left);
 	display_tree(node->right);
 }
