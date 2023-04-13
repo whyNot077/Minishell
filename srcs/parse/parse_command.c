58581ebc926b10b605a8567f58629830e9f8b365
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-13 21:22:11 +0900

    parse command

--- srcs/parse/parse_command.c
+++ srcs/parse/parse_command.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 19:08:19 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/13 20:56:04 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/13 21:21:55 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,8 @@ tree_node	*parse_command(t_token *tokens, int *index)
 	{
 		node = parse_commands(tokens, index);
 		next_token = &tokens[*index];
-		if (next_token_is_option(tokens, *index))
-		{
-			node->left = parse_commands(tokens, index);
-			next_token = &tokens[*index];
-		}
-		if (next_token_is_env(tokens, *index))
+		if (next_token_is_option(tokens, *index) \
+			|| next_token_is_env(tokens, *index))
 		{
 			node->left = parse_commands(tokens, index);
 			next_token = &tokens[*index];
