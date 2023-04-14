68139f67cb9a1472983eed326dc3c15664b5a848
Minju Kim <minkim3@c8r4s6.42seoul.kr>
2023-04-14 14:42:26 +0900

    norminette

--- srcs/parse/parse_suffix.c
+++ srcs/parse/parse_suffix.c
@@ -6,11 +6,26 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 14:15:27 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/14 14:15:35 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/14 14:41:38 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+t_tree_node	*parse_next_cmd_suffix_element(t_token *tokens, int *index)
+{
+	t_tree_node	*temp_node;
+
+	if (next_token_is_io_redirect(tokens, *index))
+	{
+		temp_node = parse_io_redirect(tokens, index);
+		if (!temp_node)
+			return (NULL);
+	}
+	else
+		temp_node = parse_commands(tokens, index);
+	return (temp_node);
+}
 
 t_tree_node	*parse_cmd_suffix(t_token *tokens, int *index)
 {
@@ -21,23 +36,14 @@ t_tree_node	*parse_cmd_suffix(t_token *tokens, int *index)
 	while (next_token_is_io_redirect(tokens, *index)
 		|| tokens[*index].type == WORD)
 	{
-		if (next_token_is_io_redirect(tokens, *index))
+		temp_node = parse_next_cmd_suffix_element(tokens, index);
+		if (!temp_node)
 		{
-			temp_node = parse_io_redirect(tokens, index);
-			if (!temp_node)
-			{
-				free_tree_nodes(node);
-				return (NULL);
-			}
-		}
-		else
-		{
-			temp_node = parse_commands(tokens, index);
+			free_tree_nodes(node);
+			return (NULL);
 		}
 		if (!node)
-		{
 			node = temp_node;
-		}
 		else
 		{
 			temp_node->left = node;
