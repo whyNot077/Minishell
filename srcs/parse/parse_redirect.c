c23e3148df313db669bdfffbe3b39323f8178c3a
Minju Kim <minkim3@c8r4s6.42seoul.kr>
2023-04-14 15:09:19 +0900

    norminette

--- srcs/parse/parse_redirect.c
+++ srcs/parse/parse_redirect.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 14:13:22 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/14 14:54:37 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/14 15:09:05 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,52 +75,6 @@ t_tree_node	*parse_io_redirect(t_token *tokens, int *index)
 			return (NULL);
 		}
 		node->right = parse_commands(tokens, index);
-	}
-	else
-	{
-		printf("Error: syntax error near unexpected token %s\n", \
-				tokens[*index].value);
-		return (NULL);
-	}
-	return (node);
-}
-
-static t_tree_node	*create_io_redirect_node_and_check(t_token *tokens, \
-	int *index)
-{
-	t_tree_node	*node;
-
-	node = create_new_node(&tokens[*index]);
-	node->type = tokens[*index].type;
-	(*index)++;
-	if (tokens[*index].value == NULL)
-	{
-		free(node);
-		printf("Error: syntax error near unexpected token %s\n", \
-			tokens[*index - 1].value);
-		return (NULL);
-	}
-	else if (tokens[*index].type != WORD)
-	{
-		free(node);
-		printf("Error: syntax error near unexpected token %s\n", \
-				tokens[*index].value);
-		return (NULL);
-	}
-	node->right = parse_commands(tokens, index);
-	return (node);
-}
-
-t_tree_node	*parse_io_redirect(t_token *tokens, int *index)
-{
-	t_tree_node	*node;
-
-	if (tokens[*index].type == REDIRECT_OUT \
-		|| tokens[*index].type == REDIRECT_IN \
-		|| tokens[*index].type == REDIRECT_APPEND \
-		|| tokens[*index].type == HEREDOC)
-	{
-		node = create_io_redirect_node_and_check(tokens, index);
 	}
 	else
 	{
