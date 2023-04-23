3c894fde2e4e2455ecb71116528f11350ba8b21d
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-23 19:47:04 +0900

    norminette

--- srcs/parse/parse_and_or.c
+++ srcs/parse/parse_and_or.c
@@ -6,35 +6,31 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 19:36:02 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/23 18:10:50 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/23 19:46:50 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void parse_and_or(t_binarytree *tree, t_token *tokens, int *index)
+void	parse_and_or(t_binarytree *tree, t_token *tokens, int *index)
 {
-    t_tree_node *rightmost_node;
-    t_tree_node *command_node;
+	t_tree_node	*rightmost_node;
+	t_tree_node	*command_node;
 
-    if (tree->root == NULL)
-    {
-        printf("Syntax error: tree is empty.\n");
+	if (tree->root == NULL)
+	{
+		printf("Syntax error: tree is empty.\n");
 		(*index)++;
-        return;
-    }
-
-    rightmost_node = find_rightmost_node(tree->root);
-
-    if (rightmost_node->type == AND || rightmost_node->type == OR)
-    {
-        printf("Syntax error: rightmost node is AND/OR.\n");
+		return ;
+	}
+	rightmost_node = find_rightmost_node(tree->root);
+	if (rightmost_node->type == AND || rightmost_node->type == OR)
+	{
+		printf("Syntax error: rightmost node is AND/OR.\n");
 		(*index)++;
-        return;
-    }
-
-    command_node = create_new_node(tokens[*index].value, tokens[*index].type);
-    rightmost_node->right = command_node;
-    (*index)++;
+		return ;
+	}
+	command_node = create_new_node(tokens[*index].value, tokens[*index].type);
+	rightmost_node->right = command_node;
+	(*index)++;
 }
-
