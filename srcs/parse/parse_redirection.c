6a429f824d39759a7b466c5eb5d396870bb8d5fb
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-23 20:34:25 +0900

    norminette

--- srcs/parse/parse_redirection.c
+++ srcs/parse/parse_redirection.c
@@ -6,67 +6,71 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 13:24:15 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/23 19:36:11 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/23 20:33:58 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void parse_filename(t_tree_node	*new_node, char *value, int *index)
+void	parse_filename(t_tree_node *new_node, char *value, int *index)
 {
 	new_node->filename = value;
-    (*index)++;
+	(*index)++;
 }
-
 
 /*
 ** if the tree is empty, add the command node to the root
 ** else, find the rightmost node.
-** If the rightmost node is of type WORD, BUILTIN, add the command node to the left of the rightmost node.
-** else, store the previous rightmost node as the left child of the new command node,
+** If the rightmost node is of type WORD, BUILTIN,
+**	add the command node to the left of the rightmost node.
+** else,
+**	store the previous rightmost node as the left child of the new command node,
 ** and the command node should be the rightmost node.
 */
-static void redirection_to_tree(t_binarytree *tree, t_tree_node *new_node)
+static void	connect_redirection_node(t_binarytree *tree, t_tree_node *current, \
+		t_tree_node *previous, t_tree_node *new_node)
 {
-    t_tree_node *current;
-    t_tree_node *previous;
-
-    if (tree->root == NULL)
-    {
-        tree->root = new_node;
-        return;
-    }
-    else
-    {
-        current = tree->root;
-        previous = NULL;
-        while (current->right)
-        {
-            previous = current;
-            current = current->right;
-        }
-
-        if (current->type == WORD || current->type == BUILTIN)
-        {
-			new_node->left = current->left;
-            current->left = new_node;
-        }
-        else
-        {
-            new_node->left = current;
-            if (previous)
-            {
-                previous->right = new_node;
-            }
-            else
-            {
-                tree->root = new_node;
-            }
-        }
-    }
+	if (current->type == WORD || current->type == BUILTIN)
+	{
+		new_node->left = current->left;
+		current->left = new_node;
+	}
+	else
+	{
+		new_node->left = current;
+		if (previous)
+		{
+			previous->right = new_node;
+		}
+		else
+		{
+			tree->root = new_node;
+		}
+	}
 }
 
+static void	redirection_to_tree(t_binarytree *tree, t_tree_node *new_node)
+{
+	t_tree_node	*current;
+	t_tree_node	*previous;
 
+	if (tree->root == NULL)
+	{
+		tree->root = new_node;
+		return ;
+	}
+	else
+	{
+		current = tree->root;
+		previous = NULL;
+		while (current->right)
+		{
+			previous = current;
+			current = current->right;
+		}
+		connect_redirection_node(tree, current, previous, new_node);
+	}
+}
 
 void	parse_redirection(t_binarytree *tree, t_token *tokens, int *index)
 {
