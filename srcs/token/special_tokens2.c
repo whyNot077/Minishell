863635e1ef31684f24518d631fa3a0357e4646dc
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-24 19:05:03 +0900

    remove function : unexpected parameter

--- srcs/token/special_tokens2.c
+++ srcs/token/special_tokens2.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 14:13:45 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/12 17:25:57 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/24 19:03:30 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,14 +36,6 @@ void	num_of_special_tokens_and_space(t_token *tokens, \
 		if (is_space_token(tokens[i].value))
 			(*space)++;
 	}
-}
-
-int	is_unexpected_parameter(char c)
-{
-	return (c == '(' || c == ')' || c == '\\' || c == '^' \
-		|| c == '*' || c == '+' || c == ';' || c == '#'\
-		|| c == '{' || c == '}' || c == '[' || c == ']' \
-		|| c == '@' || c == '!' || c == '%');
 }
 
 void	fill_data(t_process_input_data *data, const char *input, \
