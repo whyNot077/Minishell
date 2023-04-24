8dbf48da2c3a370a7b343064a9e657c32196bb27
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-04-24 14:54:19 +0900

    environment variables

--- srcs/parse/replace_env_key_to_value.c
+++ srcs/parse/replace_env_key_to_value.c
@@ -6,13 +6,32 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 13:47:12 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/24 14:23:23 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/24 14:53:39 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static char	*get_the_key(char **value, size_t i)
+static char	*find_the_key_in_double_quote(char **value, size_t i)
+{
+	size_t	start;
+	size_t	end;
+	char	*key;
+
+	start = i + 1;
+	end = start;
+	while ((*value)[end] && !is_space((*value)[end]) && \
+		(*value)[end] != '\"')
+		end++;
+	key = (char *)malloc(end - start + 1);
+	if (!key)
+		return (NULL);
+	ft_memcpy(key, *value + start, end - start);
+	key[end - start] = '\0';
+	return (key);
+}
+
+static char	*get_the_key(char **value, size_t i, int in_double_quote)
 {
 	size_t	start;
 	size_t	end;
@@ -20,6 +39,8 @@ static char	*get_the_key(char **value, size_t i)
 
 	if (!value || !*value)
 		return (NULL);
+	if (in_double_quote == 1)
+		return (find_the_key_in_double_quote(value, i));
 	start = i + 1;
 	end = start;
 	while ((*value)[end] && !is_space((*value)[end]) && (*value)[end] != '$' \
@@ -33,36 +54,37 @@ static char	*get_the_key(char **value, size_t i)
 	return (key);
 }
 
-static char	*get_env_value(char *key, char **env, size_t *i)
+static char	*get_env_value(char *key, char **env, size_t i)
 {
 	char	*env_value;
 	size_t	key_length;
 
 	key_length = ft_strlen(key);
-	while (env[*i])
+	while (env[i])
 	{
-		if (ft_strncmp(key, env[*i], key_length) == 0
-			&& env[*i][key_length] == '=')
+		if (ft_strncmp(key, env[i], key_length) == 0
+			&& env[i][key_length] == '=')
 		{
-			env_value = env[*i] + key_length + 1;
+			env_value = env[i] + key_length + 1;
 			return (ft_strdup(env_value));
 		}
-		(*i)++;
+		(i)++;
 	}
 	return (ft_strdup(""));
 }
 
-void	replace_key_in_the_string(char **value, char *key, char *env_value, size_t *i)
+void	replace_key_in_the_string(char **value, char *key, char *env_value, \
+		size_t *i)
 {
 	char	*prefix;
 	char	*suffix;
 	char	*tmp;
 
 	if (!value || !*value || !key || !env_value || !i)
-		return;
-
-	prefix = ft_substr(*value, 0, *i - 1);
-	suffix = ft_substr(*value, *i + ft_strlen(key), ft_strlen(*value) - (*i + ft_strlen(key)));
+		return ;
+	prefix = ft_substr(*value, 0, *i);
+	suffix = ft_substr(*value, *i + ft_strlen(key) + 1, \
+		ft_strlen(*value) - (*i + ft_strlen(key) + 1));
 	tmp = ft_strjoin(prefix, env_value);
 	free(env_value);
 	free(*value);
@@ -72,28 +94,34 @@ void	replace_key_in_the_string(char **value, char *key, char *env_value, size_t 
 	free(tmp);
 }
 
+
 void	replace_env_key_to_value(char **value, char **env)
 {
 	size_t	i;
 	char	*key;
 	char	*env_value;
 	int		in_single_quote;
+	int		in_double_quote;
 
 	if (value == NULL || *value == NULL)
 		return ;
 	i = -1;
 	in_single_quote = -1;
+	in_double_quote = -1;
 	while ((*value)[++i])
 	{
 		if ((*value)[i] == '\'')
 			in_single_quote *= -1;
+		else if ((*value)[i] == '\"')
+			in_double_quote *= -1;
 		if ((*value)[i] == '$' && in_single_quote == -1)
 		{
-			key = get_the_key(value, i);
-			env_value = get_env_value(key, env, &i);
+			key = get_the_key(value, i, in_double_quote);
+			env_value = get_env_value(key, env, 0);
+			printf("key: %s\n", key);
+			printf("env_value: %s\n", env_value);
 			replace_key_in_the_string(value, key, env_value, &i);
 			free(key);
 		}
 	}
 }
-
