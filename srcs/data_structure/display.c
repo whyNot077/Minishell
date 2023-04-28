31dfe2e99f82781c0bab103c1bf0da0acd6c9e06
Hyojong Choi <bluedog129@gmail.com>
2023-04-28 14:29:03 +0900

    display

--- srcs/data_structure/display.c
+++ srcs/data_structure/display.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   display.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 14:16:53 by minkim3           #+#    #+#             */
-/*   Updated: 2023/04/23 19:29:16 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/04/28 14:28:49 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,45 +35,6 @@ const char	*node_type_to_str(int node_type)
 	return ("UNKNOWN");
 }
 
-void display_tree_helper(t_tree_node *node, int depth, char *branch)
-{
-    if (node == NULL)
-        return;
-    for (int i = 0; i < depth; ++i)
-        printf("  ");
-    printf("%s", branch);
-    printf("%s, ", node_type_to_str(node->type));
-    if (node->value)
-    {
-        printf("execute = ");
-        if (node->type == WORD)
-        {
-            if (node->command)
-            {
-                for (int i = 0; node->command[i]; ++i)
-                {
-                    printf("(%s)", node->command[i]);
-                }
-            }
-            else
-            {
-                printf("(%s)", node->value);
-            }
-        }
-        else
-		{
-            printf("(%s)", node->value);
-			if (node->filename)
-				printf("filename = (%s)", node->filename);
-		}
-    }
-    printf("\n");
-    if (node->left)
-        display_tree_helper(node->left, depth + 1, "|--  left,  ");
-    if (node->right)
-        display_tree_helper(node->right, depth + 1, "`--  right,  ");
-}
-
 void inorder_traverse(t_tree_node *node)
 {
     if (node == NULL)
@@ -82,7 +43,7 @@ void inorder_traverse(t_tree_node *node)
     }
 
 	inorder_traverse(node->left);
-
+	printf("%s, ", node_type_to_str(node->type));
 	printf("execute = ");
 	if (node->type == WORD)
 	{
@@ -112,8 +73,6 @@ void inorder_traverse(t_tree_node *node)
 
 void	display_tree(t_tree_node *node)
 {
-	printf("\ndisplay tree\n");
-	display_tree_helper(node, 0, "");
 	printf("\ninorder traverse\n");
 	inorder_traverse(node);
 }
