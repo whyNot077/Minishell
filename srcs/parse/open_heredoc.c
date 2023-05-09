d1a0c16b20141199ee579757a37d465f6f23e764
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-05-09 19:36:22 +0900

    heredoc

--- srcs/parse/open_heredoc.c
+++ srcs/parse/open_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 15:41:14 by minkim3           #+#    #+#             */
-/*   Updated: 2023/05/09 19:17:52 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/05/09 19:35:02 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,39 +34,18 @@ char	*get_new_filename(const char *base, int index)
 	return (filename);
 }
 
-char	*get_dir_route(const char *base)
-{
-	char	*dir;
-	char	*dir_base;
-	char	cwd[PATH_MAX];
-
-	if (getcwd(cwd, sizeof(cwd)) == NULL)
-	{
-		printf("getcwd() error");
-		exit(1);
-	}
-	dir = ft_strjoin(cwd, "/tmp/");
-	dir_base = ft_strjoin(dir, base);
-	free(dir);
-	return (dir_base);
-}
 char	*make_unique_filename(const char *base)
 {
 	int		index;
 	char	*filename;
-	char	*dir_base;
 
 	index = 0;
 	filename = NULL;
-	dir_base = get_dir_route(base);
 	while (1)
 	{
-		filename = get_new_filename(dir_base, index);
+		filename = get_new_filename(base, index);
 		if (!file_exists(filename))
-		{
-			free(dir_base);
 			return (filename);
-		}
 		index++;
 		free(filename);
 	}
@@ -82,7 +61,7 @@ static void	get_heredoc(t_tree_node *node, char *eof)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 	{
-		printf("open error\n");
+		perror("open");
 		exit(1);
 	}
 	while (1)
