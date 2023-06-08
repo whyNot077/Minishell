271459e0c75b4bdf0cb68da3cafaf5f337aa19d9
Hyojong Choi <bluedog129@gmail.com>
2023-06-08 17:39:26 +0900

    norminette

--- srcs/execute/pipe/pipe_apply.c
+++ srcs/execute/pipe/pipe_apply.c
@@ -3,14 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_apply.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 21:09:55 by hyojocho          #+#    #+#             */
-/*   Updated: 2023/06/08 15:21:59 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/06/08 17:38:54 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+static void	built_in_child_process(t_execute *exe_tool, char **args)
+{
+	if (exe_tool->open_error == TRUE)
+		exit(0);
+	if (exe_tool->prev_fd > 0)
+	{
+		dup2(exe_tool->prev_fd, STDIN_FILENO);
+		close(exe_tool->prev_fd);
+	}
+	if (exe_tool->curr_pipe_flag == TRUE && exe_tool->outfile_fd == 0)
+	{
+		dup2(exe_tool->pipe_fd[1], STDOUT_FILENO);
+		close(exe_tool->pipe_fd[1]);
+	}
+	if (exe_tool->pipe_fd[0] > 0)
+		close(exe_tool->pipe_fd[0]);
+	built_in(args, exe_tool);
+	exit(0);
+}
 
 void	apply_built_in_pipe(char **args, t_execute *exe_tool)
 {
@@ -22,24 +42,7 @@ void	apply_built_in_pipe(char **args, t_execute *exe_tool)
 	pid = fork();
 	exe_tool->last_pid = pid;
 	if (pid == 0)
-	{
-		if (exe_tool->open_error == TRUE)
-			exit(0);
-		if (exe_tool->prev_fd > 0 )
-		{
-			dup2(exe_tool->prev_fd, STDIN_FILENO);
-			close(exe_tool->prev_fd);
-		}
-		if (exe_tool->curr_pipe_flag == TRUE && exe_tool->outfile_fd == 0)
-		{
-			dup2(exe_tool->pipe_fd[1], STDOUT_FILENO);
-			close(exe_tool->pipe_fd[1]);
-		}
-		if (exe_tool->pipe_fd[0] > 0)
-			close(exe_tool->pipe_fd[0]);
-		built_in(args, exe_tool);
-		exit(0);
-	}
+		built_in_child_process(exe_tool, args);
 	else
 	{
 		close(exe_tool->pipe_fd[1]);
