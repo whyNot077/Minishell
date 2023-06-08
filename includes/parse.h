7dff7c586b0db9c067a88a1fe920180b606348e9
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-06-08 17:08:54 +0900

    norminette

--- includes/parse.h
+++ includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 19:09:54 by minkim3           #+#    #+#             */
-/*   Updated: 2023/06/08 11:41:12 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/06/08 17:08:42 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,16 +33,18 @@ int				open_heredoc(t_tree_node *node);
 char			*make_unique_filename(const char *previous_filename);
 void			get_rightmost_and_previous(t_binarytree *tree, \
 				t_tree_node **rightmost, t_tree_node **previous);
-t_token	*		parse_dollar_sign(t_token *tokens, int index, char **env);
+t_token			*parse_dollar_sign(t_token *tokens, int index, char **env);
 void			copy_tokens_to_new_tokens(t_token *new_tokens, t_token *tokens, \
 				t_token *env_tokens, size_t index);
 void			put_it_on_the_top_of_the_rightmost_node(t_binarytree *tree, \
-				t_tree_node *rightmost, t_tree_node *previous, t_tree_node *target);
+				t_tree_node *rightmost, t_tree_node *previous, \
+				t_tree_node *target);
 void			put_it_on_the_left_of_the_rightmost_node(t_binarytree *tree, \
-				t_tree_node *rightmost, t_tree_node *previous, t_tree_node *target);
+				t_tree_node *rightmost, t_tree_node *previous, \
+				t_tree_node *target);
 void			put_it_on_the_right_of_the_rightmost_node(t_binarytree *tree, \
-				t_tree_node *rightmost, t_tree_node *previous, t_tree_node *target);
+				t_tree_node *rightmost, t_tree_node *previous, \
+				t_tree_node *target);
 char			*is_str_in_the_value(char *value);
-
 
 #endif
